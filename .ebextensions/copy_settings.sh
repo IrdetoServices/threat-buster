045d77e61a2e7c721b3c23cd4e0fbf370d330701
Ben Gidley <ben@gidley.co.uk>
2017-08-16 12:05:24 +0100

    Moving NPM to manual install

--- .ebextensions/copy_settings.sh
+++ .ebextensions/copy_settings.sh
@@ -2,4 +2,6 @@
 echo $CONFIG_BUCKET
 
 # Param1 has in it environment name according to docs
-aws s3 cp s3://$CONFIG_BUCKET/local_settings.py portal/local_settings.py
+# Must check s3 region as in some regions calling without --region fails
+REGION=`aws s3api get-bucket-location --bucket development.threat-buster.com --output text`
+aws s3 cp s3://$CONFIG_BUCKET/local_settings.py portal/local_settings.py --region $REGION
