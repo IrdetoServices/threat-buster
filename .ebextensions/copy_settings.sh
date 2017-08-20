0deeee1a4c31154ba8b1cd42a0b3da6faa9f8c76
Ben Gidley <ben@gidley.co.uk>
2017-08-20 08:36:16 +0100

    config bucked fix

--- .ebextensions/copy_settings.sh
+++ .ebextensions/copy_settings.sh
@@ -2,5 +2,6 @@
 echo $CONFIG_BUCKET
 
 # Must check s3 region as in some regions calling without --region fails
+CONFIG_BUCKET=`sudo ./get-config environment | jq -r '.CONFIG_BUCKET'`
 REGION=`aws s3api get-bucket-location --bucket $CONFIG_BUCKET --output text`
 aws s3 cp s3://$CONFIG_BUCKET/local_settings.py portal/local_settings.py --region $REGION
