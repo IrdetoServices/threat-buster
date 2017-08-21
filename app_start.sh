bf1a7f1d01218f2299f8ad54497f17e3a744cebd
Ben Gidley <ben@gidley.co.uk>
2017-08-21 09:34:34 +0100

    Fixing app start

--- app_start.sh
+++ app_start.sh
@@ -8,10 +8,10 @@ if [ -n "$CONFIG_BUCKET" ]; then
     REGION=`aws s3api get-bucket-location --bucket $CONFIG_BUCKET --output text`
     aws s3 cp s3://$CONFIG_BUCKET/local_settings.py portal/local_settings.py --region $REGION
 
-    manage.py migrate --noinput
-    manage.py createsu
-    manage.py bower_install --allow-root
-    manage.py collectstatic --noinput
+    ./manage.py migrate --noinput
+    ./manage.py createsu
+    ./manage.py bower_install --allow-root
+    ./manage.py collectstatic --noinput
 fi
 
 /uwsgi-start.sh
