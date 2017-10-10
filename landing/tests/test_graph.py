dc895c67bc1ba7498bea16ed634e937a877ee809
Ben Gidley <ben@gidley.co.uk>
2017-10-10 11:57:35 +0100

    Added Unit tests for REST API

--- landing/tests/test_graph.py
+++ landing/tests/test_graph.py
@@ -1,7 +1,7 @@
 from uuid import uuid4
 
 from django.test import TestCase
-from neomodel import db, clear_neo4j_database
+from neomodel import db
 
 from landing.graph_models import AttackGoal, AttackMethod, Mitigation, BasicEvent, Tenant, Attacker
 
@@ -25,60 +25,63 @@ __copyright__ = """
 __license__ = "Apache 2.0"
 
 
-class GraphTestCase(TestCase):
-    def setUp(self):
-        self.tenant = Tenant(tenant='tests{}'.format(uuid4().urn)).save()
+class GraphTestMixin:
+    def create_tenant(self):
+        return Tenant(tenant='tests{}'.format(uuid4().urn)).save()
 
-    def tearDown(self):
+    def destroy_tenant_and_data(self, tenant):
         results, meta = db.cypher_query(
-            "MATCH (n:Tenant)-[*]->(m) WHERE n.tenant={tenant} DETACH delete (m) DETACH DELETE (n)",
-            params={'tenant': self.tenant.tenant})
-        self.assertIsNotNone(results)
+            "MATCH (n:Tenant)-[*]->(m) WHERE n.tenant={tenant} DETACH DELETE (m)",
+            params={'tenant': tenant.tenant})
+        results, meta = db.cypher_query(
+            "MATCH (n:Tenant) WHERE n.tenant={tenant} DETACH DELETE (n)",
+            params={'tenant': tenant.tenant})
 
-    def test_assembling_attack_tree(self):
+    def create_test_attack_goal(self, tenant):
         attack_goal = AttackGoal(title="Steal Lawnmower", attack_cost=1000, impact_attack_cost=5000).save()
-        self.assertIsNotNone(attack_goal)
-        self.tenant.attack_trees.connect(attack_goal)
-
+        tenant.attack_trees.connect(attack_goal)
         attack_method_1 = AttackMethod(title="Break Into Storage", goal_rules="A").save()
         attack_goal.attack_methods.connect(attack_method_1)
         attack_method_2 = AttackMethod(title="Steal from Operator", goal_rules="O").save()
         attack_goal.attack_methods.connect(attack_method_2)
-
         basic_event_1 = BasicEvent(title="Break open door", attack_cost=10, technical_skill=30).save()
         attack_method_1.basic_events.connect(basic_event_1)
         mitigation_1 = Mitigation(title="Padlock", attack_cost=50, technical_skill=50).save()
         attack_method_1.mitigations.connect(mitigation_1)
-
         basic_event_2 = BasicEvent(title="Hold up at gunpoint", attack_cost=500, technical_skill=20).save()
         attack_method_2.basic_events.connect(basic_event_2)
         mitigation_2 = Mitigation(title="Guard Dog", attack_cost=1000, technical_skill=80).save()
         attack_method_2.mitigations.connect(mitigation_2)
-
         attack_method_3 = AttackMethod(title="Many options method", goal_rules="O").save()
         attack_goal.attack_methods.connect(attack_method_3)
-
         attack_method_3a = AttackMethod(title="Many options method a", goal_rules="A").save()
         attack_method_3.attack_methods.connect(attack_method_3a)
-
         basic_event_3a = BasicEvent(title="Event 3a", attack_cost=10, technical_skill=30).save()
         attack_method_3a.basic_events.connect(basic_event_3a)
         mitigation_3a = Mitigation(title="Mitigation 3a", attack_cost=50, technical_skill=50).save()
         attack_method_3a.mitigations.connect(mitigation_3a)
-
         attack_method_3b = AttackMethod(title="Many options method b", goal_rules="A").save()
         attack_method_3.attack_methods.connect(attack_method_3b)
-
         basic_event_3b = BasicEvent(title="Event 3a", attack_cost=10, technical_skill=30).save()
         attack_method_3b.basic_events.connect(basic_event_3b)
         mitigation_3b = Mitigation(title="Mitigation 3a", attack_cost=50, technical_skill=50).save()
         attack_method_3b.mitigations.connect(mitigation_3b)
-
         attacker_strong = Attacker(title="Mafia", attack_cost=1000, technical_skill=80).save()
         attack_goal.attackers.connect(attacker_strong)
-
         attacker_weak = Attacker(title="Chancer", attack_cost=5, technical_skill=10).save()
         attack_goal.attackers.connect(attacker_weak)
+        return attack_goal, attacker_strong, attacker_weak
+
+
+class GraphTestCase(TestCase, GraphTestMixin):
+    def setUp(self):
+        self.tenant = self.create_tenant()
+
+    def tearDown(self):
+        self.destroy_tenant_and_data(self.tenant)
+
+    def test_assembling_attack_tree(self):
+        attack_goal, attacker_strong, attacker_weak = self.create_test_attack_goal(self.tenant)
 
         # Results as strings
         results, meta = db.cypher_query(
